1050b51ba8b8298c2ee55709507596b0f1f45347
Patience Lartey <pattlartey@yahoo.com>
2021-01-05 10:53:55 -0500

    replaced DB with url string

--- 4-natours/server.js
+++ 4-natours/server.js
@@ -1,10 +1,48 @@
+const mongoose = require('mongoose')
 const dotenv = require('dotenv');
+// dotenv.config({ path: "./config.env" })
 const app = require('./app');
 // console.log(app.get('env'));
-dotenv.config({ path: './config.env' });
-// console.log(process.env);
 
-const port = process.env.PORT || 5005;
+// console.log(process.env);
+// const DB = process.env.DATABASE.replace(
+//   '<PASSWORD>',
+//   process.env.DATABASE_PASSWORD
+// );
+
+// mongoose.connect("mongodb://localhost:27017/natours")
+mongoose.connect("mongodb+srv://pcellich:dzidedi@cluster0.zpsot.mongodb.net/Natours?retryWrites=true&w=majority", { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true })
+  .then(() => {
+    // console.log(con.connections)
+    console.log("DB connection successful")
+  });
+const tourSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'A tour must have a name'],
+    unique: true
+  },
+  rating: {
+    type: Number,
+    default: 4.5
+  },
+  price: {
+    type: Number,
+    required: [true, 'A tour must have a price']
+  }
+
+
+})
+
+const Tour = mongoose.model("Tour", tourSchema)
+const testTour = new Tour({
+  name: "The Forest Hiker",
+  rating: 4.7,
+  price: 497
+})
+testTour.save()
+
+const port = 5005;
 app.listen(port, () => {
   console.log(`App running on port ${port}`);
-});
+})
--- querySelector.js
+++ querySelector.js
@@ -1,1 +1,2 @@
 db.tours.find({ price: { $lte: 500 }, rating: { $gte: 4.8 } });
+// mongo "mongodb+srv://cluster0.qbxao.mongodb.net/natours" --username pcellich --password dzidedi
